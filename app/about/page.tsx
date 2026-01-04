9393387c746f7db21c1b550bb35a5d9b3201afb8
Julianna Messineo <juliannamessineo@gmail.com>
2026-01-04 17:02:58 -0600

    Add padding to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import About from '@/components/About';
 
 function AboutPage({}) {
   return (
-    <div className="flex flex-col items-center mt-20">
+    <div className="flex flex-col items-center mt-20 px-4 pb-10">
       <About />
     </div>
   );
