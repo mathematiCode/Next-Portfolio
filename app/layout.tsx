ebe5cceea0784a13cf3d91bfb69ac924b4796048
Julianna Messineo <juliannamessineo@gmail.com>
2026-01-05 15:30:30 -0600

    delete hotjar script, using npm package instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,22 +37,6 @@ export default function RootLayout({
             `,
           }}
         />
-        <Script
-          id="hotjar-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:6613364,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-          }}
-        />
       </body>
     </html>
   );
